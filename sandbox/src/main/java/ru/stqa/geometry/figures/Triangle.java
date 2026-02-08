34911a2a26de6486450a45b8f4caacc24bc9cca1
@ahalak <scsi1234@yandex.ru>
2026-02-09 00:58:55 +0300

    Создание классов и перемещение их по пакетам

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,11 @@
+package ru.stqa.geometry.figures;
+
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
